c9e455890e5a90da818477fdf6f2d1291dec2bff
Kyle <kylpeacock@gmail.com>
2017-08-25 15:51:03 -0700

    working configuration for google hosting

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production';
 const next = require('next');
 const pathMatch = require('path-match')
@@ -19,8 +18,11 @@ app.prepare().then(() => {
         return handle(req, res);
     })
 
-    server.listen(port, (err) => {
+    const handler = server.listen(8080, (err) => {
         if (err) throw err;
-        console.log(`Server ready on http://localhost:${port}`);
+        const host = handler.address().address;
+        const port = handler.address().port;
+
+        console.log(`Example app listening at http://${host}:${port}`);
     })
 })
